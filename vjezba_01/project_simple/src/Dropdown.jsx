133c4a46e4e56da45876c66945978430de8c30d2
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-03-20 09:43:28 +0100

    simple deepseek check

---
+++ vjezba_01/project_simple/src/Dropdown.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Dropdown = ({ options, value, onChange }) => {
+  return (
+    <select 
+      value={value} 
+      onChange={(e) => onChange(e.target.value)}
+      style={{ padding: "10px", fontSize: "16px", width: "250px" }}
+    >
+      {options.map((option, index) => (
+        <option key={index} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
