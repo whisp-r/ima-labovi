380978f326cb32efba3df94cbdeab123d5cb551a
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-03-20 10:59:47 +0100

    split into 2

--- vjezba_01/project_simple/src/Dropdown.jsx
+++ vjezba_01/project_simple/src/Dropdown.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Dropdown = ({ options, value, onChange }) => {
   return (
-    <select 
-      value={value} 
+    <select
+      value={value}
       onChange={(e) => onChange(e.target.value)}
-      style={{ padding: "10px", fontSize: "16px", width: "250px" }}
+      style={{
+        padding: "10px",
+        fontSize: "16px",
+        width: "250px",
+      }}
     >
       {options.map((option, index) => (
         <option key={index} value={option}>
